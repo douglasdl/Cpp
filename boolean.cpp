f6205366604b1686ec599a275b4c583d86ca225c
Douglas <douglas_san@hotmail.com>
2019-11-15 16:17:37 +0900

    Add files via upload

---
+++ boolean.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  bool isCodingFun = true;
+  bool isFishTasty = false;
+  cout << isCodingFun << "\n";
+  cout << isFishTasty << "\n";
+
+  int x = 10;
+  int y = 9;
+  cout << (x > y);
+
+  return 0;
+}
