a61d77b8da52b1d3f231a5f900e50be8a8b467bf
Douglas <douglas_san@hotmail.com>
2019-11-15 16:20:48 +0900

    Add files via upload

---
+++ userInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x; 
+  // insertion operator (<<)
+  cout << "Type a number: "; // Type a number and press enter
+  // extraction operator (>>)
+  cin >> x; // Get user input from the keyboard
+  cout << "Your number is: " << x << "\n"; // Display the input value
+  
+
+  return 0;
+}
